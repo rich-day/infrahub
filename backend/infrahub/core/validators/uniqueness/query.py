226afdd38a0f26dd8b9e94c60022114d5362fe86
jeremy brisson <jeremy@opsmill.com>
2025-04-18 16:33:54 +0200

    rebase and upgrade neo4j 5.26.5

--- backend/infrahub/core/validators/uniqueness/query.py
+++ backend/infrahub/core/validators/uniqueness/query.py
@@ -164,16 +164,15 @@ class NodeUniqueAttributeConstraintQuery(Query):
         # ruff: noqa: E501
         query = """
         // get attributes for node and its relationships
-        CALL {
+        CALL () {
             %(select_subqueries_str)s
             RETURN %(return_subqueries_str)s
         }
-        CALL {
+        CALL () {
             %(filter_subqueries_str)s
         }
         WITH start_node, potential_path, rel_identifier, potential_attr, potential_attr_value
-        CALL {
-            WITH potential_path
+        CALL (potential_path) {
             WITH potential_path  // workaround for neo4j not allowing WHERE in a WITH of a subquery
             // only the branches and times we care about
             WHERE all(
@@ -194,8 +193,7 @@ class NodeUniqueAttributeConstraintQuery(Query):
             start_node,
             rel_identifier,
             potential_attr
-        CALL {
-            WITH enriched_paths
+        CALL (enriched_paths) {
             UNWIND enriched_paths as path_to_check
             RETURN path_to_check[0] as current_path, path_to_check[4] as latest_value
             ORDER BY
@@ -205,16 +203,14 @@ class NodeUniqueAttributeConstraintQuery(Query):
                 path_to_check[3] DESC
             LIMIT 1
         }
-        CALL {
+        CALL (current_path) {
             // only active paths
-            WITH current_path
             WITH current_path  // workaround for neo4j not allowing WHERE in a WITH of a subquery
             WHERE all(r IN relationships(current_path) WHERE r.status = "active")
             RETURN current_path as active_path
         }
-        CALL {
+        CALL (active_path) {
             // get deepest branch name
-            WITH active_path
             UNWIND %(branch_name_and_level)s as branch_name_and_level
             RETURN branch_name_and_level[0] as branch_name
             ORDER BY branch_name_and_level[1] DESC
